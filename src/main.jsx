44969744fd3f52e1ca162ad6c7d80b1636f4051b
a b <ab@example.com>
2024-12-12 01:19:11 +0200

    talandinbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,34 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 import App from "./App";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
+const theme = createTheme({
+  direction: "rtl",
+});
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 root.render(
   <StrictMode>
-    <App />
-
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <div dir="rtl">
+        <App />
+        </div>
+        
+      </ThemeProvider>
+    </CacheProvider>
   </StrictMode>
 );
--- src/template/LineMatching.jsx
+++ src/template/LineMatching.jsx
@@ -1,93 +1,260 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./LineMatching.css"; // סטיילינג לקומפוננטה
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Swal from "sweetalert2";
+import { Typography } from "@mui/material";
 
 const LineMatching = () => {
+  const [question1, setQuestion1] = React.useState("");
+  const [question2, setQuestion2] = React.useState("");
+  const [question3, setQuestion3] = React.useState("");
+  const [question4, setQuestion4] = React.useState("");
+  const [question5, setQuestion5] = React.useState("");
+  const [showAnswer, setShowAnswer] = React.useState({
+    question1: false,
+    question2: false,
+    question3: false,
+    question4: false,
+    question5: false,
+  });
+  const [isClicked, setIsClicked] = React.useState(false);
+
+  useEffect(() => {
+    console.log(showAnswer);
+  }, [showAnswer]);
 
   const checkAnswers = () => {
-    console.log("i love my life");
-    console.log(document.getElementById("que1").selected);
-    for(let i=1;i<6;i++) {
-      console.log(document.getElementById(que1).selected);
-        if(document.getElementById(`que${i}`).selected==true) {
-            document.getElementById(`format${i}`).style.color="green";
-        } else {
-            document.getElementById(`format${i}`).style.color="red";
-        }
-    }
-   document.getElementById("check").style.display = "none";
-   document.getElementById("countinue").style.display = "block";
-};
+    setIsClicked(true);
 
+    if (question1 === data.marshal) {
+      setShowAnswer((prev) => ({ ...prev, question1: true }));
+      console.log("hilotuli");
+    }
+    if (question2 === data.tium) {
+      setShowAnswer((prev) => ({ ...prev, question2: true }));
+    }
+    if (question3 === data.agam) {
+      setShowAnswer((prev) => ({ ...prev, question3: true }));
+    }
+    if (question4 === data.hamal) {
+      setShowAnswer((prev) => ({ ...prev, question4: true }));
+    }
+    if (question5 === data.adracha) {
+      setShowAnswer((prev) => ({ ...prev, question5: true }));
+    }
+    if (question1 && question2 && question3 && question4 && question5) {
+      Swal.fire({
+        title: "כל הכבוד",
+        text: "סיימת את הלומדה! אתם לגמרי יכולים להיות גאים בעצמכם",
+        icon: "success",
+        heightAuto: false,
+      });
+    }
+  };
+  const data = {
+    marshal: "מרשל",
+    tium: "מחלקת תיאום",
+    agam: 'קצין אג"ם',
+    hamal: 'חמל ביה"ס',
+    adracha: 'מפל"ג הדרכה/מפקד ביה"ס',
+  };
   return (
-    <div className="line-matching-container">
-       <div className="select" id="ques1">
-       <label>
-      דרישת אמצעית נבצע דרך ה:
-      <select name="selectedFruit" id="format1">
-        <option selected disabled>לחץ לבחירה</option>
-        <option value="false">מחלקת תיאום</option>
-        <option value="true" >מרשל</option>
-        <option value="false">קצין אג"ם</option>
-        <option value="false">חמל ביה"ס</option>
-        <option value="false">מפלג הדרכה/מפקד ביה"ס</option>
-      </select>
-    </label>
-    </div><br />
-    <div className="select" id="ques2">
-    <label>
-      תיאום שטחים נבצע דרך ה:
-      <select name="selectedFruit" id="format2">
-        <option selected disabled>לחץ לבחירה</option>
-        <option value="true">מחלקת תיאום</option>
-        <option value="false" >מרשל</option>
-        <option value="false">קצין אג"ם</option>
-        <option value="false">חמל ביה"ס</option>
-        <option value="false">מפלג הדרכה/מפקד ביה"ס</option>
-      </select>
-    </label>
-    </div><br />
-    <div className="select" id="ques3">
-    <label>
-    גרף ביה"ס נבצע בעזרת:
-      <select name="selectedFruit" id="format3">
-        <option selected disabled>לחץ לבחירה</option>
-        <option value="false">מחלקת תיאום</option>
-        <option value="false" >מרשל</option>
-        <option value="true">קצין אג"ם</option>
-        <option value="false">חמל ביה"ס</option>
-        <option value="false">מפלג הדרכה/מפקד ביה"ס</option>
-      </select>
-    </label>
-    </div><br />
-    <div className="select" id="ques4">
-    <label>
-זימון מילואים נבצע בעזרת:
-      <select name="selectedFruit" id="format4">
-        <option selected disabled>לחץ לבחירה</option>
-        <option value="false">מחלקת תיאום</option>
-        <option value="false" >מרשל</option>
-        <option value="false">קצין אג"ם</option>
-        <option value="true">חמל ביה"ס</option>
-        <option value="false">מפלג הדרכה/מפקד ביה"ס</option>
-      </select>
-    </label>
-    </div><br />
-    <div className="select" id="ques5">
-    <label>
-       הצגת אישור תוכניות נבצע בעזרת:
-      <select name="selectedFruit" id="format5">
-        <option selected disabled>לחץ לבחירה</option>
-        <option value="false">מחלקת תיאום</option>
-        <option value="false" >מרשל</option>
-        <option value="false">קצין אג"ם</option>
-        <option value="false">חמל ביה"ס</option>
-        <option value="true">מפלג הדרכה/מפקד ביה"ס</option>
-      </select>
-    </label>
-    </div><br />
-    <button id = "check" onClick={() => checkAnswers()}>בדיקה</button>
-    {/* <button id = "countinue" onClick={() => nextQuestion}>המשך</button> */}
-    </div>
+    <Box
+      gap={2}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+    >
+      <Typography color="#152a49" fontSize={"5vmin"} fontFamily={"abraham"}>
+        אנא בחרו עבור כל משימה את מי שתצטרכו על מנת לבצע אותה
+      </Typography>
+      <Box key="que1" sx={{ minWidth: 350 }}>
+        <FormControl fullWidth >
+          <InputLabel>דרישת אמצעים</InputLabel>
+          <Select
+            variant="filled"
+            value={question1}
+            label="Age"
+            onChange={(event) => setQuestion1(event.target.value)}
+            
+          >
+            <MenuItem className="rt" value={data.marshal}>
+              {data.marshal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.tium}>
+              {data.tium}
+            </MenuItem>
+            <MenuItem className="rt" value={data.agam}>
+              {data.agam}
+            </MenuItem>
+            <MenuItem className="rt" value={data.hamal}>
+              {data.hamal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.adracha}>
+              {data.adracha}
+            </MenuItem>
+          </Select>
+        </FormControl>
+        {isClicked &&
+        (showAnswer.question1 ? (
+          <div className="feedback-answer">תשובה נכונה</div>
+        ) : (
+          <div className="feedback-answer-error">תשובה שגויה אנא נסו שנית</div>
+        ))}
+      </Box>
+
+      
+
+      <Box key="que2" sx={{ minWidth: 350 }}>
+        <FormControl fullWidth>
+          <InputLabel>תיאום שטחים</InputLabel>
+          <Select
+            variant="filled"
+            value={question2}
+            label="Age"
+            onChange={(event) => setQuestion2(event.target.value)}
+          >
+            <MenuItem className="rt" value={data.marshal}>
+              {data.marshal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.tium}>
+              {data.tium}
+            </MenuItem>
+            <MenuItem className="rt" value={data.agam}>
+              {data.agam}
+            </MenuItem>
+            <MenuItem className="rt" value={data.hamal}>
+              {data.hamal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.adracha}>
+              {data.adracha}
+            </MenuItem>
+          </Select>
+        </FormControl>
+        {isClicked &&
+        (showAnswer.question2 ? (
+          <div className="feedback-answer">תשובה נכונה</div>
+        ) : (
+          <div className="feedback-answer-error">תשובה שגויה אנא נסו שנית</div>
+        ))}
+      </Box>
+
+    
+
+      <Box key="que3" sx={{ minWidth: 350 }}>
+        <FormControl fullWidth>
+          <InputLabel>גרף ביה"ס</InputLabel>
+          <Select
+            variant="filled"
+            value={question3}
+            label="Age"
+            onChange={(event) => setQuestion3(event.target.value)}
+          >
+            <MenuItem className="rt" value={data.marshal}>
+              {data.marshal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.tium}>
+              {data.tium}
+            </MenuItem>
+            <MenuItem className="rt" value={data.agam}>
+              {data.agam}
+            </MenuItem>
+            <MenuItem className="rt" value={data.hamal}>
+              {data.hamal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.adracha}>
+              {data.adracha}
+            </MenuItem>
+          </Select>
+        </FormControl>
+        {isClicked &&
+        (showAnswer.question3 ? (
+          <div className="feedback-answer">תשובה נכונה</div>
+        ) : (
+          <div className="feedback-answer-error">תשובה שגויה אנא נסו שנית</div>
+        ))}
+      </Box>
+
+     
+
+      <Box key="que4" sx={{ minWidth: 350 }}>
+        <FormControl fullWidth>
+          <InputLabel>זימון מילואים</InputLabel>
+          <Select
+            variant="filled"
+            value={question4}
+            label="Age"
+            onChange={(event) => setQuestion4(event.target.value)}
+          >
+            <MenuItem className="rt" value={data.marshal}>
+              {data.marshal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.tium}>
+              {data.tium}
+            </MenuItem>
+            <MenuItem className="rt" value={data.agam}>
+              {data.agam}
+            </MenuItem>
+            <MenuItem className="rt" value={data.hamal}>
+              {data.hamal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.adracha}>
+              {data.adracha}
+            </MenuItem>
+          </Select>
+        </FormControl>
+        {isClicked &&
+        (showAnswer.question4 ? (
+          <div className="feedback-answer">תשובה נכונה</div>
+        ) : (
+          <div className="feedback-answer-error">תשובה שגויה אנא נסו שנית</div>
+        ))}
+      </Box>
+
+      
+
+      <Box key="que5" sx={{ minWidth: 350 }}>
+        <FormControl fullWidth>
+          <InputLabel>הצגת אישור תוכניות</InputLabel>
+          <Select
+            variant="filled"
+            value={question5}
+            label="Age"
+            onChange={(event) => setQuestion5(event.target.value)}
+          >
+            <MenuItem className="rt" value={data.marshal}>
+              {data.marshal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.tium}>
+              {data.tium}
+            </MenuItem>
+            <MenuItem className="rt" value={data.agam}>
+              {data.agam}
+            </MenuItem>
+            <MenuItem className="rt" value={data.hamal}>
+              {data.hamal}
+            </MenuItem>
+            <MenuItem className="rt" value={data.adracha}>
+              {data.adracha}
+            </MenuItem>
+          </Select>
+        </FormControl>
+        {isClicked &&
+        (showAnswer.question5 ? (
+          <div className="feedback-answer">תשובה נכונה</div>
+        ) : (
+          <div className="feedback-answer-error">תשובה שגויה אנא נסו שנית</div>
+        ))}
+      </Box>
+
+      <br />
+      <button id="checkBtn" onClick={checkAnswers}>בדיקה</button>
+    </Box>
   );
 };
 
